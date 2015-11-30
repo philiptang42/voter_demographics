cbcd204b0b4a2b2a04e6b97b47859a95e9ff0725
philiptang42 <philiptang42@gmail.com>
2015-11-29 23:57:31 -0500

    set initial values

--- voter_demographics.rb
+++ voter_demographics.rb
@@ -9,3 +9,13 @@ demographics = [
     education: 'College'
   }
 ]
+
+age_total = 0
+income_total = 0
+household_size_total = 0
+male_total = 0
+female_total = 0
+unspecified_gender_total = 0
+college_total = 0
+high_school_total = 0
+no_high_school_total = 0
