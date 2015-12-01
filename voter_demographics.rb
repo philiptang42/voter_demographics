b8541e073e5cffe7209b2bed35d19e8b9557670a
philiptang42 <philiptang42@gmail.com>
2015-11-30 22:24:22 -0500

    hash data filled

--- voter_demographics.rb
+++ voter_demographics.rb
@@ -18,76 +18,76 @@ demographics = [
     education: 'High School'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name:'Sam',
+    last_name: 'Farelly',
+    age: 32,
+    income: 80000,
+    household_size: 2,
+    gender: 'Unspecified',
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Joan',
+    last_name: 'Favreau',
+    age: 35
+    income: 65000,
+    household_size: 4,
+    gender: 'Female',
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Sam',
+    last_name: 'McNulty',
+    age: 38,
+    income: 63000,
+    household_size: 3,
+    gender: 'Male',
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Mark',
+    last_name: 'Minahan',
+    age: 38,
+    income: 63000,
+    household_size: 3,
+    gender: 'Male',
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Susan',
+    last_name: 'Umani',
+    age: 45,
+    income: 75000,
+    household_size: 2,
+    gender: 'Female',
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Bill',
+    last_name: 'Perault',
+    age: 24,
+    income: 45000,
+    household_size: 1,
+    gender: 'Male',
+    education: 'Did Not Complete High School'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Doug',
+    last_name: 'Stamper',
+    age: 45,
+    income: 75000,
+    household_size: 1,
+    gender: 'Male'
+    education: 'College'
   },
   {
-    first_name:
-    last_name:
-    age:
-    income:
-    household_size:
-    gender:
-    education:
+    first_name: 'Francis',
+    last_name: 'Underwood',
+    age: 52,
+    income: 100000,
+    household_size: 2,
+    gender: 'Male',
+    education: 'College'
   }
 ]
 
